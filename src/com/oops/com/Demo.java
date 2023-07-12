fbebd8b3b93ea49cd8e2487029f7858eb40c5789
venkata49 <vvkumar207@gmail.com>
2023-07-12 15:11:53 +0530

    Initial Commit.

---
+++ src/com/oops/com/Demo.java
@@ -0,0 +1,18 @@
+package com.oops.com;
+
+class Demo{
+	public static void main(String[] args){
+	
+	//create sub class objects....
+	sub1 s1 = new sub1();
+	sub2 s2 = new sub2();
+	sub3 s3 = new sub3();
+	
+	//let the objects call and use calculate() methods...
+	s1.calculate(3);
+	s2.calculate(4);
+	s3.calculate(5);
+	
+	
+	}
+}
